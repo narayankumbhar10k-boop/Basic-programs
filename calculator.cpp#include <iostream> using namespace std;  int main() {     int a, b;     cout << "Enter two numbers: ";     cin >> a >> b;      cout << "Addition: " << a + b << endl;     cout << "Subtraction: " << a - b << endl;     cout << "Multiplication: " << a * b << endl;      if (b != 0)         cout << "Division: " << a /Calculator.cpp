d317b1fb29393d306fcb4365d8d00663eba6f418
narayankumbhar10k-boop <narayankumbhar10k@gmail.com>
2026-04-14 20:10:21 +0530

    Calculator.cpp

---
+++ calculator.cpp#include <iostream> using namespace std;  int main() {     int a, b;     cout << "Enter two numbers: ";     cin >> a >> b;      cout << "Addition: " << a + b << endl;     cout << "Subtraction: " << a - b << endl;     cout << "Multiplication: " << a * b << endl;      if (b != 0)         cout << "Division: " << a /Calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Addition: " << a + b << endl;
+    cout << "Subtraction: " << a - b << endl;
+    cout << "Multiplication: " << a * b << endl;
+
+    if (b != 0)
+        cout << "Division: " << a / b << endl;
+    else
+        cout << "Division not possible" << endl;
+
+    return 0;
+}
